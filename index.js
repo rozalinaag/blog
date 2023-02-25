94907c0dff8bfe9cd1ca30957dea4e2a54200eb1
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2023-02-25 22:25:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Сервер запущен на localhost:4444');
+});
