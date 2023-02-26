99bf45b821a23599b2e4bbda874147e465547d52
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2023-02-26 22:35:53 +0300

    connected to mongoDB, added model,  validation

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import { auth } from './private.js';
+import { password } from './private.js';
+
+import { registerValidator } from './validations/auth.js';
+import { validationResult } from 'express-validator';
+
+mongoose
+  .connect(
+    `mongodb+srv://${auth}:${password}@cluster0.vqazijs.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log('DB Ok');
+  })
+  .catch((err) => {
+    console.log('DB error', err);
+  });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.post('/auth/register', registerValidator, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(4444, (err) => {
